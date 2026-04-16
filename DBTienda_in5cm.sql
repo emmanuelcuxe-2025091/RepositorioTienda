346a87bf3b29699bee79190b52fb5ed3f4d3cabf
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-04-15 23:36:02 -0600

    feat: Agrega nuevos endpoints, CRUD dinámico y cambio de estilo en la home

--- DBTienda_in5cm.sql
+++ DBTienda_in5cm.sql
@@ -381,11 +381,11 @@ call sp_usuarios_create('e', '1', 'jperez@mail.com', 'admin', 1);
 call sp_usuarios_create('mlopez', 'pass1234', 'mlopez@mail.com', 'user', 1);
 call sp_usuarios_create('cmartinez', 'pass1234', 'cmartinez@mail.com', 'user', 1);
 call sp_usuarios_create('agarcia', 'pass1234', 'agarcia@mail.com', 'user', 1);
-call sp_usuarios_create('lhernandez', 'pass1234', 'lhernandez@mail.com', 'admin', 1);
+call sp_usuarios_create('lhernandez', 'pass1234', 'lhernandez@mail.com', 'user', 1);
 call sp_usuarios_create('sramirez', 'pass1234', 'sramirez@mail.com', 'user', 1);
 call sp_usuarios_create('dtorres', 'pass1234', 'dtorres@mail.com', 'user', 1);
 call sp_usuarios_create('vflores', 'pass1234', 'vflores@mail.com', 'user', 1);
-call sp_usuarios_create('rjimenez', 'pass1234', 'rjimenez@mail.com', 'admin', 1);
+call sp_usuarios_create('rjimenez', 'pass1234', 'rjimenez@mail.com', 'user', 1);
 call sp_usuarios_create('gmorales', 'pass1234', 'gmorales@mail.com', 'user', 1);
 
 -- Productos --
